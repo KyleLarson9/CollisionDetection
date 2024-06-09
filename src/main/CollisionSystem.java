0ffa165a1c8bff15900001037fd8a27497ec3ba1
KyleLarson9 <Kylelarson08@gmail.com>
2024-06-09 15:37:43 -0500

    Cleaned up code to start lineRect collision detection

--- src/main/CollisionSystem.java
+++ src/main/CollisionSystem.java
@@ -9,26 +9,24 @@ import java.util.ArrayList;
 public class CollisionSystem {
 	
 	private Sim sim;
-	
-	private ArrayList<Point2D.Float> intersectionPoints;
-	
+		
 	private float intersectionX = -1;
 	private float intersectionY = -1;
 	
 	public CollisionSystem(Sim sim) {
 		this.sim = sim;
-		this.intersectionPoints = new ArrayList<>();
 	}
 	
 	// public methods
 	
 	public void render(Graphics2D g2d) {
-		renderIntersectionPoints(g2d);
+		renderLineIntersectionPoints(g2d);
+		renderLineRectIntersectionPoints(g2d);
 	}
 	
 	// private methods
 	
-	private void renderIntersectionPoints(Graphics2D g2d) {
+	private void renderLineIntersectionPoints(Graphics2D g2d) {
 		float x1 = Sim.SIM_WIDTH/2;
 		float y1 = Sim.SIM_HEIGHT/2;
 		float x2 = sim.mouseInputs.getX();
@@ -45,20 +43,16 @@ public class CollisionSystem {
 		g2d.setColor(Color.cyan);
 		g2d.draw(new Line2D.Double(x3, y3, x4, y4));
 		
-		float x5 = 100;
-		float y5 = 200;
-		float x6 = 400; 
-		float y6 = 500;		
+		boolean hit = predictedLineLineCollision(x1, y1, x2, y2, x3, y3, x4, y4);
 		
-		g2d.draw(new Line2D.Double(x5, y5, x6, y6));
-		
-		//boolean hit = lineLineCollision(x1, y1, x2, y2, x3, y3, x4, y4);
-		boolean hit2 = predictedLineLineCollision(x1, y1, x2, y2, x5, y5, x6, y6);
-		
-		if(hit2 == true) {
+		if(hit == true) {
 			g2d.setColor(Color.red);
 			g2d.fillOval((int) intersectionX - 5,(int) intersectionY - 5,(int) 10,(int) 10);
 		}
+		
+	}
+	
+	private void renderLineRectIntersectionPoints(Graphics2D g2d) {
 		
 	}
 	
@@ -68,17 +62,18 @@ public class CollisionSystem {
 		boolean intersected = true;
 		float denominator = (y4-y3)*(x2-x1)-(x4-x3)*(y2-y1);
 		
-		if(denominator == 0) { // parallel
+		if(denominator == 0) { // check if parallel
 			intersected = false;
 		}
 		
+	    // Calculate the parameters t and u for the intersection formulas
 		float t = ((x4-x3)*(y1-y3)-(y4-y3)*(x1-x3))/denominator;
 		float u = ((x2-x1)*(y1-y3)-(y2-y1)*(x1-x3))/denominator;
 		
+	    // Check if the intersection point lies on the second line segment
 		if(t >= 0 && t <= 1 && u >= 0 && u <= 1) {
 			intersectionX = x1 + t*(x2-x1);
 			intersectionY = y1 + t*(y2-y1);
-			
 			intersected = true;
 		} else {
 			intersected = false;
@@ -93,10 +88,11 @@ public class CollisionSystem {
 		boolean intersected = true;
 		float denominator = (y4-y3)*(x2-x1)-(x4-x3)*(y2-y1);
 		
-		if(denominator == 0) { // parallel
+		if(denominator == 0) { // check if parallel
 			intersected = false;
 		}
 		
+	    // Calculate the parameters t and u for the intersection formulas
 		float t = ((x4-x3)*(y1-y3)-(y4-y3)*(x1-x3))/denominator;
 		float u = ((x2-x1)*(y1-y3)-(y2-y1)*(x1-x3))/denominator;
 		
@@ -104,12 +100,11 @@ public class CollisionSystem {
 		intersectionX = x1 + t*(x2-x1);
 		intersectionY = y1 + t*(y2-y1);
 		
-		// distance from each point on line 1 to intersection point
+	    // Calculate distances from the intersection point to the endpoints of the first line segment
 		float d1 = (float) Math.sqrt(Math.pow((intersectionX - x2), 2) + Math.sqrt(Math.pow((intersectionY - y2), 2)));
 		float d2 = (float) Math.sqrt(Math.pow((intersectionX - x1), 2) + Math.sqrt(Math.pow((intersectionY - y1), 2)));
 		
-		
-		
+	    // Check if the intersection point lies on the second line segment
 		if(x3 == x4) { // vertical lines
 			if(intersectionY >= y3 && intersectionY <= y4 && d1 < d2) {
 				intersected = true;
@@ -121,11 +116,8 @@ public class CollisionSystem {
 		} else {
 			intersected = false;
 		}
-		
-		// vertical lines
-		
+				
 		return intersected;
-		
 	}
 	
 	private boolean lineRectangleCollision(float x1, float y1, float x2, float y2) {
